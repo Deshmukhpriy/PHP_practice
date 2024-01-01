80ba118116319c7a43d5d3ba0c95c9b3af4f4c76
Deshmukhpriy <148950686+Deshmukhpriy@users.noreply.github.com>
2024-01-01 11:05:49 +0530

    delete_student.php

--- delete_student.php
+++ delete_student.php
@@ -3,10 +3,10 @@
 $con = mysqli_connect('localhost', 'root', '', 'my_crud');
 
 if (isset($_GET['id'])) {
-    $userId = $_GET['id'];
-    $deleteUser = mysqli_query($con, "DELETE FROM `user` WHERE id = $userId");
+    $studentId = $_GET['id'];
+    $deleteStudent = mysqli_query($con, "DELETE FROM `student` WHERE id = $userId");
     if ($deleteUser) {
-        header('Location: user_list.php');
+        header('Location: student_list.php');
     } else {
         echo "Not able to delete";
     }
