15723f693ab97c1baa8f0b4a5fac05f2f4f699fa
Deshmukhpriy <148950686+Deshmukhpriy@users.noreply.github.com>
2024-01-01 12:53:12 +0530

    edit_student.php

--- edit_student.php
+++ edit_student.php
@@ -5,8 +5,8 @@ $con = mysqli_connect('localhost', 'root', '', 'my_crud');
 if (isset($_GET['id'])) {
     $userId = $_GET['id'];
 
-    $selectUser = mysqli_query($con, "SELECT * FROM `user` WHERE id = $userId");
-    $edituser = mysqli_fetch_assoc($selectUser);
+    $selectStudent = mysqli_query($con, "SELECT * FROM `student` WHERE id = $userId");
+    $editStudent = mysqli_fetch_assoc($selectStudent);
 }
 
 if (isset($_POST['submit'])) {
@@ -20,12 +20,12 @@ if (isset($_POST['submit'])) {
     $zip = $_POST['zip'];
   
 
-    $updateData = mysqli_query($con, "UPDATE `user` SET `full_name`='$full_name',`contact`='$contact',`email`='$email',`birth_date`='$birth_date',`address`='$address',`city`='$city',`state`='$state',`zip`='$zip' WHERE id = $userId");
+    $updateData = mysqli_query($con, "UPDATE `student` SET `full_name`='$full_name',`contact`='$contact',`email`='$email',`birth_date`='$birth_date',`address`='$address',`city`='$city',`state`='$state',`zip`='$zip' WHERE id = $userId");
 
     if ($updateData) {
-        header('Location: user_list.php');
+        header('Location: student_list.php');
     } else {
-        echo "User not updated";
+        echo "Student not updated";
     }
 
 }
@@ -49,41 +49,41 @@ if (isset($_POST['submit'])) {
       <form action="#" class="form" method="POST">
         <div class="input-box">
           <label>Full Name</label>
-          <input type="text" placeholder="Enter full name" name="full_name" value="<?php echo $edituser['full_name'] ?>" required />
+          <input type="text" placeholder="Enter full name" name="full_name" value="<?php echo $editStudent['full_name'] ?>" required />
         </div>
 
         <div class="input-box">
           <label>Email Address</label>
-          <input type="text" placeholder="Enter email address" name="email" value="<?php echo $edituser['email'] ?>" required />
+          <input type="text" placeholder="Enter email address" name="email" value="<?php echo $editStudent['email'] ?>" required />
         </div>
 
         <div class="column">
           <div class="input-box">
             <label>Phone Number</label>
-            <input type="number" placeholder="Enter phone number" name="contact" value="<?php echo $edituser['contact'] ?>" required />
+            <input type="number" placeholder="Enter phone number" name="contact" value="<?php echo $editStudent['contact'] ?>" required />
           </div>
           <div class="input-box">
             <label>Birth Date</label>
-            <input type="date" placeholder="Enter birth date" name="birth_date" value="<?php echo $edituser['birth_date'] ?>" required />
+            <input type="date" placeholder="Enter birth date" name="birth_date" value="<?php echo $editStudent['birth_date'] ?>" required />
           </div>
         </div>
         
         <div class="input-box address">
           <label>Address</label>
-          <input type="text" placeholder="Enter street address" name="address" value="<?php echo $edituser['address'] ?>" required />
+          <input type="text" placeholder="Enter street address" name="address" value="<?php echo $editStudent['address'] ?>" required />
          <div class="column">
             <div class="select-box">
-              <select name="state" value="<?php echo $edituser['state'] ?>">
+              <select name="state" value="<?php echo $editStudent['state'] ?>">
                 <option hidden>State</option>
                 <option>Maharastra</option>
                 
               </select>
             </div>
-            <input type="text" placeholder="Enter your city" name="city" value="<?php echo $edituser['city'] ?>" required />
+            <input type="text" placeholder="Enter your city" name="city" value="<?php echo $editStudent['city'] ?>" required />
           </div>
           <div class="column">
            
-            <input type="number" placeholder="Enter postal code" name="zip" value="<?php echo $edituser['zip'] ?>" required />
+            <input type="number" placeholder="Enter postal code" name="zip" value="<?php echo $editStudent['zip'] ?>" required />
           </div>
         </div>
         <button type="submit" name="submit">Submit</button>
